9c20d8fde2e68e276a3e3f935c41180d41c4e384
JasonOnes <jasonr.jones14@gmail.com>
2018-07-10 11:02:19 -0700

    mistake total

--- hangman.py
+++ hangman.py
@@ -35,57 +35,75 @@ def start_from_rando():
 def in_game(all_words, index_cw, guesses_made):
     ''' main game play, built with params '''
     
-    wordkey = all_words[int(index_cw)]
-    word = word_dict[wordkey]
-    guesses = guesses_made #decoded 
+    word = get_current_word(all_words, index_cw)
+   
+    guesses = guesses_made #TODO decode here 
     whole_word_guessed = False
     for guess in guesses_made:
         if guess == "6":
             whole_word_guessed = True
-    print("&&&&&&&&&&&&&&&&&&")
-    print(guesses)
+
     display_word = shown_word(word, guesses)
     
-    mistakes = 0
+    mistakes = []
     
     if "_" not in display_word:
         print("YOU WIN")
+        remaining = 8 - int(index_cw) 
         
-        return render_template('congrats.html', remaining=all_words, word_completed=index_cw)
+        return render_template('congrats.html', remaining=remaining,
+                                all_words=all_words,
+                                word_completed=index_cw,
+                                guesses_made=guesses_made)
     
+    print(guesses_made)
+    for guess in guesses_made:
+        if guess.lower() not in word.lower():
+            mistakes.append(guess.lower())
+    # reminder that mistakes will always contain 1
+    mistake_total = len(mistakes) - 1
+    print(mistake_total)
+    if mistake_total == 9:
+        return render_template('loser.html')
+
     if request.method == 'POST':
         
         let_guess = request.form['letter_guess']
         print(let_guess)
-        if let_guess.lower() not in word.lower():
-            mistakes += 1
-        print(str(mistakes))
+       
         guesses_made = let_guess + guesses_made #encode let_guess here
-        print(guesses_made)
-            
+        
+        
         return redirect(url_for('.in_game', 
                                 all_words=all_words, 
                                 index_cw=index_cw, 
                                 guesses_made=guesses_made))
+    print("!!!!!!!!!!!!!!!!")
+
+    print(mistakes)
     return render_template('game-page.html', 
-                            all_words=all_words, 
-                            index_cw=index_cw, 
-                            guesses_made=guesses, 
-                            word=display_word, 
-                            word_guessed=whole_word_guessed)
+                                all_words=all_words, 
+                                index_cw=index_cw, 
+                                guesses_made=guesses, 
+                                word=display_word, 
+                                word_guessed=whole_word_guessed,
+                                mistake_total=mistake_total)
 
 @app.route("/<all_words>/<index_cw>/<guesses_made>/<t>", methods=['POST', 'GET'])                    
 def word_guess(all_words, index_cw, guesses_made, t):
     if request.method == 'POST':
         word_guess = request.form['word_guess']
-        #if word_guess.lower() == word_dict[index_cw].lower():
         if word_guess.lower() == get_current_word(all_words, index_cw):
             print("you won")
-            return render_template('congrats.html', remaining=all_words, word_completed=index_cw)
+            remaining = 8 - int(index_cw)
+            return render_template('congrats.html', remaining=remaining, 
+                                                    all_words=all_words,
+                                                    word_completed=index_cw, 
+                                                    guesses_made=guesses_made)
         whole_word_guessed = True
         guesses = "6" + guesses_made
-        wordkey = all_words[int(index_cw)]
-        w = word_dict[wordkey]
+        w = get_current_word(all_words, index_cw)
+        
         display_word = shown_word(w, guesses)
         return render_template('game-page.html', 
                                 all_words=all_words, 
@@ -97,24 +115,7 @@ def word_guess(all_words, index_cw, guesses_made, t):
 @app.route("/new_game/<all_words>/<word_completed>", methods=['POST', 'GET'])
 def next_game(all_words, word_completed):
     ''' starts new game from words left '''
-    # if all_words[0] in d_lets:
-    #     print("GAME is totally complete!!")
-    #     return render_template('congrats.html', remaining=None)
-    # index_cw = rando_word_pick()
-    #adds dummy value to make up for removed word key
-    # print("$$$$$$$$$$$$$$$$$")
-    # print(len(all_words))
-    # how_many_to_add = 9 - (len(all_words))
-    # foolin = ''
-    # for i in range(how_many_to_add):
-    #     foolin += add_on()
-    #     i += 1
-    # print(len(foolin))
-    # word_keys.remove(index_cw)
-    # words_keys_for_display = word_keys[:]
-    # words_keys_for_display.append(str(foolin))
-   
-    # all_words = ''.join(words_keys_for_display)
+
     index = int(word_completed) + 1
     index_cw = str(index)
     guesses = '1'
@@ -126,28 +127,29 @@ def next_game(all_words, word_completed):
 
 @app.route("/<all_words>/<index_cw>/<guesses_made>/<s>/<q>", methods=['GET', 'POST'])
 def undo(all_words, index_cw, guesses_made, s, q):
-    
-    print("JJLKJLJLJLK")
+    ''' undoes the last action, if last action was a new game call, recalls last word,
+        until first word, then returns to intro page, NOT a back button'''
+
     if len(guesses_made) <= 1:
         while int(index_cw) > 0:
             prev_index = str(int(index_cw) - 1)
             old_word = get_current_word(all_words, prev_index)
             display_word = shown_word(old_word, "1")
             whole_word_guessed = False
+            
             return render_template('game-page.html', 
                                     all_words=all_words, 
                                     index_cw=prev_index, 
-                                    guesses_made=guesses_made, 
+                                    guesses_made="1", 
                                     word=display_word, 
                                     word_guessed=whole_word_guessed)
         if int(index_cw) == 0:
             return render_template('/intro.html')
-    #prev_all_words = all_words[1:]
+  
     prev_guesses_made = guesses_made[1:]
 
-    wordkey = all_words[int(index_cw)]
-    w = word_dict[wordkey]
-    
+    w = get_current_word(all_words, index_cw)
+
     display_word = shown_word(w, prev_guesses_made)
 
     whole_word_guessed = False
